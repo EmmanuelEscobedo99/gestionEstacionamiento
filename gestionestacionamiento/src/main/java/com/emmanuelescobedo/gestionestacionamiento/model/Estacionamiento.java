a402726e7ff30fe63d1aae7a772929e376df6244
EmmanuelEscobedo99 <emmanuelescobedo099@gmail.com>
2026-08-19 17:04:54 -0600

    modelo Estacionamiento

--- gestionestacionamiento/src/main/java/com/emmanuelescobedo/gestionestacionamiento/model/Estacionamiento.java
+++ gestionestacionamiento/src/main/java/com/emmanuelescobedo/gestionestacionamiento/model/Estacionamiento.java
@@ -1,7 +1,22 @@
 package com.emmanuelescobedo.gestionestacionamiento.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.math.BigDecimal;
 
 @Entity
 public class Estacionamiento {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long codeEstacionamiento;
+    private String nombre;
+    private String direccion;
+    private String ciudad;
+    private Integer capacidadTotal;
+    private BigDecimal tarifaHora;
+    private boolean activo;
 }
