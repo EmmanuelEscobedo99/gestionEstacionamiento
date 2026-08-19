71fa7d96cab8e10d15df06de9353609c6e0c3b9b
EmmanuelEscobedo99 <emmanuelescobedo099@gmail.com>
2026-08-19 17:06:53 -0600

    modelo Espacio

---
+++ gestionestacionamiento/src/main/java/com/emmanuelescobedo/gestionestacionamiento/model/Espacio.java
@@ -0,0 +1,17 @@
+package com.emmanuelescobedo.gestionestacionamiento.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Espacio {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long codeEspacio;
+    private String numero;
+    private Enum tipo;
+    private boolean disponible;
+}
