4629db0947fbab235b20cd52a2601c58e23fb8d6
EmmanuelEscobedo99 <emmanuelescobedo099@gmail.com>
2026-08-21 17:22:00 -0600

    Interfaz Estacionamiento

---
+++ gestionestacionamiento/src/main/java/com/emmanuelescobedo/gestionestacionamiento/service/IEstacionamientoService.java
@@ -0,0 +1,23 @@
+package com.emmanuelescobedo.gestionestacionamiento.service;
+
+import com.emmanuelescobedo.gestionestacionamiento.model.Estacionamiento;
+
+import java.util.List;
+
+public interface IEstacionamientoService {
+
+    //METODOS CRUD
+
+    //READ
+    public List<Estacionamiento> traerEstacionamientos();
+    Estacionamiento buscarEstacionamiento(Long codeEstacionamiento);
+
+    //CREATE
+    Estacionamiento crearEstacionamiento(Estacionamiento estacionamiento);
+
+    //UPDATE
+    Estacionamiento editarEstacionamiento(Long codeEstacionamiento, Estacionamiento estacionamiento);
+
+    //DELETE
+    boolean eliminarEstacionamiento(Long codeEstacionamiento);
+}
