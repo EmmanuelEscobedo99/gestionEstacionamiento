fd08168c5a88ddf995b223f4038e598bf2561e27
EmmanuelEscobedo99 <emmanuelescobedo099@gmail.com>
2026-08-28 18:07:42 -0600

    Service de Pago creada.

---
+++ gestionestacionamiento/src/main/java/com/emmanuelescobedo/gestionestacionamiento/service/PagoService.java
@@ -0,0 +1,63 @@
+package com.emmanuelescobedo.gestionestacionamiento.service;
+
+import com.emmanuelescobedo.gestionestacionamiento.model.Pago;
+import com.emmanuelescobedo.gestionestacionamiento.repository.IPagoRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PagoService implements IPagoService{
+
+    private final IPagoRepository pagoRepo;
+
+    public PagoService(IPagoRepository pagoRepo) {
+        this.pagoRepo = pagoRepo;
+    }
+
+    @Override
+    public List<Pago> traerPago() {
+        return pagoRepo.findAll();
+    }
+
+    @Override
+    public Pago buscarPago(Long codePago) {
+        return pagoRepo.findById(codePago).orElse(null);
+    }
+
+    @Override
+    public Pago crearPago(Pago pago) {
+        if (pago == null){
+            return null;
+        }
+
+        return pagoRepo.save(pago);
+    }
+
+    @Override
+    public Pago editarPago(Long codePago, Pago pago) {
+        Pago pagoEditar = buscarPago(codePago);
+
+        if (pagoEditar == null){
+            return null;
+        }
+
+        pagoEditar.setMonto(pago.getMonto());
+        pagoEditar.setFechaPago(pago.getFechaPago());
+        pagoEditar.setMetodoPago(pago.getMetodoPago());
+
+        return pagoRepo.save(pagoEditar);
+    }
+
+    @Override
+    public boolean eliminarPago(Long codePago) {
+        Pago pagoEliminar = buscarPago(codePago);
+
+        if (pagoEliminar == null){
+            return false;
+        }
+
+        pagoRepo.delete(pagoEliminar);
+        return true;
+    }
+}
