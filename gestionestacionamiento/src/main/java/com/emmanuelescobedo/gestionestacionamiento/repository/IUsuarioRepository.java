ca1d431b4a9d43f46bcfa1e9d8d01fddb3efc146
EmmanuelEscobedo99 <emmanuelescobedo099@gmail.com>
2026-09-04 16:09:56 -0600

    Seguridad en roles

--- gestionestacionamiento/src/main/java/com/emmanuelescobedo/gestionestacionamiento/repository/IUsuarioRepository.java
+++ gestionestacionamiento/src/main/java/com/emmanuelescobedo/gestionestacionamiento/repository/IUsuarioRepository.java
@@ -3,5 +3,9 @@ package com.emmanuelescobedo.gestionestacionamiento.repository;
 import com.emmanuelescobedo.gestionestacionamiento.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface IUsuarioRepository extends JpaRepository<Usuario, Long> {
+    Optional<Usuario> findByEmail(String email);
+    boolean existsByEmail(String email);
 }
